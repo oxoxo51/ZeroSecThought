17151f80b8fa100f47eaf872cfabe2b8da388b87
oxoxo51 <y.omt0601@gmail.com>
2016-11-16 16:47:13 +0900

    herokuでのエラー回避のため桁数指定を外す

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,13 +2,13 @@
 
 # --- !Ups
 
-CREATE TABLE memo {
-    id                      bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    parent_id               bigint(20),
-    title                   varchar(100),
-    content                 varchar(2000),
+CREATE TABLE memo (
+    id                      bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    parent_id               bigint,
+    title                   varchar,
+    content                 varchar,
     create_date             date
-};
+);
 
 # --- !Downs
 
