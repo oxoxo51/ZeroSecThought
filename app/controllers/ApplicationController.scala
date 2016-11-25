fac04dd7047208f96ac15cf47a7589058e48ab1e
oxoxo51 <y.omt0601@gmail.com>
2016-11-25 19:34:20 +0900

    #3 java.sql.Dateのjson出力時変換処理(Writes)

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -2,16 +2,13 @@ package controllers
 
 import javax.inject.Inject
 
-import forms.{MemoSearchForms, MemoSearchForm}
 import models.daos.MemoDao
 import play.api.Logger
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 /**
   * Created on 16/10/14.
--- app/models/Memo.scala
+++ app/models/Memo.scala
@@ -1,6 +1,7 @@
 package models
 
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 /**
   * Created on 16/10/21.
@@ -14,6 +15,14 @@ case class Memo
   createDate: java.sql.Date
 )
 object Memo {
-  implicit def jsonWrites = Json.writes[Memo]
+  val sqlDateWrite = Writes.sqlDateWrites("yyyy/MM/dd")
+  implicit lazy val memoWrites: Writes[Memo] = (
+    (__ \ "id").write[Option[Long]] and
+    (__ \ "parentId").write[Option[Long]] and
+    (__ \ "title").write[String] and
+    (__ \ "content").write[String] and
+    (__ \ "createDate").write[java.sql.Date](sqlDateWrite)
+  )(unlift(Memo.unapply))
   implicit def jsonReads = Json.reads[Memo]
 }
+
