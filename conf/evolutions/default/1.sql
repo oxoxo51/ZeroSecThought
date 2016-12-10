7bd1743dd1eb0001605c22a6942de43403de1854
oxoxo51 <y.omt0601@gmail.com>
2016-12-10 22:57:33 +0900

    ローカルではH2で稼働できるよう設定修正

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,14 +2,14 @@
 
 # --- !Ups
 
-CREATE TABLE MEMO (
-    ID                      SERIAL NOT NULL PRIMARY KEY,
-    PARENT_ID               bigint,
-    TITLE                   varchar,
-    CONTENT                 varchar,
-    CREATE_DATE             date
+CREATE TABLE "memo" (
+    "id"                      SERIAL NOT NULL PRIMARY KEY,
+    "parent_id"               integer,
+    "title"                   varchar(30),
+    "content"                 varchar(400),
+    "create_date"             date
 );
 
 # --- !Downs
 
-DROP TABLE MEMO;
+DROP TABLE "memo";
